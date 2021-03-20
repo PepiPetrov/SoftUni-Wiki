7063f6e0b0ab110c4dfb3608e4eaab337599ab74
pepiProg <74186271+pepiProg@users.noreply.github.com>
2021-03-20 17:51:30 +0200

    Add files via upload

---
+++ SoftWiki/src/api/data.js
@@ -0,0 +1,36 @@
+import * as api from './api.js';
+
+
+const host = 'http://localhost:3030';
+api.settings.host = host;
+
+export const login = api.login;
+export const register = api.register;
+export const logout = api.logout;
+
+// Implement application-specific requests
+
+// Team Collection
+
+export async function getArticles() {
+    const teams = await api.get(host + '/jsonstore/wiki');
+    return teams;
+}
+
+export async function getArticle(id) {
+    return await api.get(host + '/jsonstore/wiki/' + id);
+}
+
+export async function createAritcle(article) {
+    const result = await api.post(host + '/jsonstore/wiki', article);
+    
+    return result;
+}
+
+export async function editArticle(id, article) {
+    return await api.put(host + '/jsonstore/wiki/' + id, article);
+}
+
+export async function deleteArticle(id) {
+    return await api.del(host + '/jsonstore/wiki/' + id);
+}
